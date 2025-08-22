745120b873a63599ad84359d6464e23d5b03be62
Sonam Shah <sonam.shah1687@gmail.com>
2025-08-22 09:29:11 -0500

    refactor: use JsonWebToken encode method in user controller and remove issue_token method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-
 	def create
 		user = User.new(user_params)
 		if user.save
-      render json: { user: { id: user.id, email: user.email}, token: issue_token(user)}, status: :created
+			token = JsonWebToken.encode({ sub: user.id , exp: 24.hours.from_now.to_i })
+      render json: { user: { id: user.id, email: user.email}, token: token}, status: :created
 		else
 			render json: { errors: user.errors }, status: :unprocessable_entity
 		end
@@ -13,10 +13,5 @@ class UsersController < ApplicationController
 
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
-	end
-
-	def issue_token(user)
-    payload = { sub: user.id, exp: 24.hours.from_now.to_i, iss: "assistant_api" }
-    JWT.encode(payload, Rails.application.credentials.secret_key_base, "HS256")
 	end
 end
