4d1c126a2f529bf8f950dd1f4f504bd0f27b00a8
Sonam Shah <sonam.shah1687@gmail.com>
2025-08-22 09:23:02 -0500

    refactor: extract JWT encode/decode in JsonWebToken service

---
+++ app/services/json_web_token.rb
@@ -0,0 +1,27 @@
+class JsonWebToken
+	ISSUER = "assistant_api"
+
+  def self.secret_key
+	  ENV["JWT_SECRET"].presence ||  Rails.application.credentials.secret_key_base
+  end
+
+	def self.encode(payload, exp: 24.hours.from_now.to_i)
+		data = payload.dup
+		data[:exp] ||= exp
+		data[:iss] ||= ISSUER
+
+		JWT.encode(data, secret_key, "HS256")
+	end
+
+	def self.decode(token)
+		return nil if token.blank?
+
+		body, = JWT.decode(token, secret_key, true, { algorithm: "HS256", iss: ISSUER, verify_issuer: true })
+		HashWithIndifferentAccess.new(body)
+	rescue JWT::ExpiredSignature
+	  raise
+	rescue JWT::DecodeError
+		raise
+	end  
+
+end
